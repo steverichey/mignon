95901a0d8ffb60f370c55a28c5f2c33e144887a3
Steve Richey <srichey@floatlearning.com>
2015-04-29 14:02:31 -0500

    switch to lime-based test structure

--- test/source/Main.hx
+++ test/source/Main.hx
@@ -1,21 +1,24 @@
 package;
 
+import lime.app.Application;
+
 import com.apple.UIKit.UIAlertController;
+import com.apple.UIKit.UIAlertAction;
 
-class Main
+class Main extends Application
 {
-	public static function main():Void
-	{
-		new Main();
-	}
-
 	public function new()
 	{
+		super();
+
+		#if ios
+		// create an alert on iOS
 		var alert = new UIAlertController("My Alert", 
-			                                "This is an alert.", 
-                                      UIControllerStyle.Alert);
+			                              "This is an alert.", 
+                                      	  UIControllerStyle.Alert);
 		var defaultAction = new UIAlertAction("OK", UIAlertActionStyle.Default);
 		alert.addAction(defaultAction);
 		alert.present(true, null);
+		#end
 	}
 }
